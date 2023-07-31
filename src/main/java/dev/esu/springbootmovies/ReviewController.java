a3be1b5f5be6dd64907d8fb21be5853bde31982f
eemb <eduardoemb1@gmail.com>
2023-07-31 18:46:23 -0300

    Update ReviewController.java
    feature(Review): add createReview POST method to its controller

--- src/main/java/dev/esu/springbootmovies/ReviewController.java
+++ src/main/java/dev/esu/springbootmovies/ReviewController.java
@@ -1,6 +1,7 @@
 package dev.esu.springbootmovies;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/movies")
+@RequestMapping("/api/v1/reviews")
 public class ReviewController {
 
     @Autowired
@@ -18,6 +19,6 @@ public class ReviewController {
 
     @PostMapping
     public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload){
-
+        return new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.CREATED);
     }
 }
